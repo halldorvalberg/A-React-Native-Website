ad1baf158aec1b22349937b54203bc85d48415fd
Halldór Valberg Skúlason <halldorvs17@ru.is>
2021-09-24 17:06:34 +0000

    To Do list added

--- app/App.js
+++ app/App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-web';
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>A React Native Website</Text>
+      <Text>Lorem Ipsum</Text>
       <StatusBar style="auto" />
-    </View>
+
+    </View>    
   );
 }
 
@@ -18,4 +23,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    
+  }
 });
